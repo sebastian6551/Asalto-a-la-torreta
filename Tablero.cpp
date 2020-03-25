55eb5112d5f8549a445990e17ff7b691c4750082
Sebastián Estupiñán <53454385+sebastian6551@users.noreply.github.com>
2020-03-25 12:38:00 -0500

    Update Tablero.cpp

--- Tablero.cpp
+++ Tablero.cpp
@@ -9,3 +9,16 @@ Tablero::~Tablero(){
 }
 
 //Métodos de la clase Tablero...
+
+/*Otiene el elemento que este en la coordenada [x][y] del tablero de juego. 
+Sirve para conocer la ubicación de los jugadores y la torreta.*/
+int Tablero::obtenerElemento(int x, int y)
+{
+  return tablero[x][y];
+}
+
+/*Modifica el valor del elemento que este en la coordenada [x][y] del tablero de juego.*/
+void Tablero::modificarElemento(int x, int y, int valor) 
+{
+  tablero[x][y] = valor;
+}
