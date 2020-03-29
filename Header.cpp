6d7508914cfd3771b02a5b1513e0d0c565331763
Johan <52992534+johandapc@users.noreply.github.com>
2020-03-29 18:22:42 -0500

    Add files via upload

---
+++ Header.cpp
@@ -0,0 +1,16 @@
+#include "Header.h"
+
+Header::Header(){
+}
+
+void Header::GUI(){
+  int opcion;
+//aquí se van añadiendo las opciones del menú
+  do{
+    cout << "Bienvenido a Code Legends!" << endl;
+    cout << "Por favor escoja una opción: " << endl;
+    cout << "1. Nueva partida." << endl;
+    cin >> opcion;
+    }
+  }
+}
---
+++ Header.h
@@ -0,0 +1,16 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+#include "Torreta.h"
+#include "Tablero.h"
+#include "Avatar.h"
+using namespace std;
+
+class Header{
+  public:
+  Header();
+  void GUI();
+
+}
+
+#endif
