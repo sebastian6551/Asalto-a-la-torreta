b1faf621891ec0bf500c27a4418ee775bae7dd85
Sebastián Estupiñán <53454385+sebastian6551@users.noreply.github.com>
2020-03-30 01:07:44 -0500

    Update Tablero.cpp
    El métodos que retorna el elemento que este en la coordenada [x][y] del tablero de juego, retorna la coordenada [x-1][y-1] porque el tablero en realidad empieza en [0][0] y va hasta [9][9], entonces este 'truco' permite que al llamar al elemento [10][10] muestre el último elemento que en realidad es el [9][9]. Lo mismo ocurre con el método de modificarElemento().

--- Tablero.cpp
+++ Tablero.cpp
@@ -8,17 +8,58 @@ Tablero::Tablero(){
 Tablero::~Tablero(){
 }
 
-//Métodos de la clase Tablero...
+/*Este método guarda el valor de cada posición de la tabla 
+que está en un archivo .txt en tablero*/
+void Tablero::configurarTablero(std::string rutaArchivo)
+{
+  ifstream abrir(rutaArchivo.c_str());
+  string datos;
+
+  int numero;
+  int espaciosHorizontales = 0;
+  int x = 0;
+  while(espaciosHorizontales < 10)
+    {
+          for(int y = 0; y < 10; y++)
+          {
+          abrir>>datos;
+          stringstream recibe(datos);
+          recibe>>numero;
+          tablero[x][y] = numero;
+          }
+          x++;
+          espaciosHorizontales++;
+    }
+}
+
+/*
+Muestra el tablero.
+*/
+void Tablero::imprimirTablero()
+{
+  int espaciosHorizontales = 0;
+  int x = 0;
+  while(espaciosHorizontales < 10)
+    {
+          for(int y = 0; y < 10; y++)
+          {
+          cout<<tablero[x][y]<<" ";
+          }
+          cout<<endl;
+          x++;
+          espaciosHorizontales++;
+    }
+}
 
 /*Otiene el elemento que este en la coordenada [x][y] del tablero de juego. 
 Sirve para conocer la ubicación de los jugadores y la torreta.*/
 int Tablero::obtenerElemento(int x, int y)
 {
-  return tablero[x][y];
+  return tablero[x-1][y-1];
 }
 
 /*Modifica el valor del elemento que este en la coordenada [x][y] del tablero de juego.*/
 void Tablero::modificarElemento(int x, int y, int valor) 
 {
-  tablero[x][y] = valor;
+  tablero[x-1][y-1] = valor;
 }
